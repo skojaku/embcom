ecabf6bfe734636cc857428d54e347c845eafd78
Sadamori Kojaku <skojaku@iu.edu>
2021-07-28 21:36:56 -0400

    wrap code as a library

---
+++ libs/embcom/embcom/embeddings.py
@@ -0,0 +1,253 @@
+"""Module for embedding"""
+import logging
+
+import faiss
+import gensim
+import networkx as nx
+import numba
+import numpy as np
+import pandas as pd
+import scipy
+from embcom import samplers, utils, rsvds
+
+from scipy import sparse, stats
+
+
+logger = logging.getLogger(__name__)
+
+
+try:
+    import glove
+except ImportError:
+    print(
+        "Ignore this message if you do not use Glove. Otherwise, install glove python package by 'pip install glove_python_binary' "
+    )
+
+#
+# Base class
+#
+class NodeEmbeddings:
+    """Super class for node embedding class."""
+
+    def __init__(self):
+        self.in_vec = None
+        self.out_vec = None
+
+    def fit(self):
+        """Estimating the parameters for embedding."""
+        pass
+
+    def transform(self, dim, return_out_vector=False):
+        """Compute the coordinates of nodes in the embedding space of the
+        prescribed dimensions."""
+        # Update the in-vector and out-vector if
+        # (i) this is the first to compute the vectors or
+        # (ii) the dimension is different from that for the previous call of transform function
+        if self.out_vec is None:
+            self.update_embedding(dim)
+        elif self.out_vec.shape[1] != dim:
+            self.update_embedding(dim)
+        return self.out_vec if return_out_vector else self.in_vec
+
+    def update_embedding(self, dim):
+        """Update embedding."""
+        pass
+
+
+class Glove:
+    def __init__(
+        self,
+        num_walks=10,
+        walk_length=40,
+        window_length=10,
+        restart_prob=0,
+        p=1.0,
+        q=1.0,
+        verbose=False,
+    ):
+        self.in_vec = None  # In-vector
+        self.out_vec = None  # Out-vector
+        self.sampler = samplers.SimpleWalkSampler(
+            num_walks,
+            walk_length,
+            window_length,
+            restart_prob,
+            p,
+            q,
+            sample_center_context_pairs=True,
+            verbose=False,
+        )
+        self.learning_rate = 0.05
+        self.w2vparams = {"epochs": 25, "no_threads": 4}
+
+    def fit(self, net):
+        A = utils.to_adjacency_matrix(net)
+        self.sampler.sampling(A)
+        center, context, freq = self.sampler.get_center_context_pairs()
+        center = center.astype(int)
+        context = context.astype(int)
+        N = self.sampler.num_nodes
+        self.cooccur = sparse.coo_matrix(
+            (freq, (center, context)), shape=(N, N), dtype="double"
+        )
+        return self
+
+    def transform(self, dim, return_out_vector=False):
+        # Update the in-vector and out-vector if
+        # (i) this is the first to compute the vectors or
+        # (ii) the dimension is different from that
+        # for the previous call of transform function
+        update_embedding = False
+        if self.out_vec is None:
+            update_embedding = True
+        elif self.out_vec.shape[1] != dim:
+            update_embedding = True
+
+        # Update the dimension and train the model
+        if update_embedding:
+            self.model = glove.Glove(
+                no_components=dim, learning_rate=self.learning_rate
+            )
+            self.model.fit(self.cooccur, **self.w2vparams)
+            self.in_vec = self.model.word_vectors
+            self.out_vec = self.model.word_vectors
+
+        if return_out_vector:
+            return self.out_vec
+        else:
+            return self.in_vec
+
+class Node2Vec(NodeEmbeddings):
+    """A python class for the node2vec.
+
+    Parameters
+    ----------
+    num_walks : int (optional, default 10)
+        Number of walks per node
+    walk_length : int (optional, default 40)
+        Length of walks
+    window_length : int (optional, default 10)
+    restart_prob : float (optional, default 0)
+        Restart probability of a random walker.
+    p : node2vec parameter (TODO: Write doc)
+    q : node2vec parameter (TODO: Write doc)
+    """
+
+    def __init__(
+        self,
+        num_walks=10,
+        walk_length=40,
+        window_length=10,
+        restart_prob=0,
+        p=1.0,
+        q=1.0,
+        verbose=False,
+        random_teleport=False,
+    ):
+        self.in_vec = None  # In-vector
+        self.out_vec = None  # Out-vector
+        self.sampler = samplers.SimpleWalkSampler(
+            num_walks,
+            walk_length,
+            window_length,
+            restart_prob,
+            p,
+            q,
+            sample_center_context_pairs=False,
+            verbose=False,
+            random_teleport=random_teleport,
+        )
+
+        self.sentences = None
+        self.model = None
+        self.verbose = verbose
+
+        self.w2vparams = {
+            "sg": 1,
+            "min_count": 0,
+            "iter": 1,
+            "workers": 4,
+        }
+
+    def fit(self, net):
+        """Estimating the parameters for embedding.
+
+        Parameters
+        ---------
+        net : nx.Graph object
+            Network to be embedded. The graph type can be anything if
+            the graph type is supported for the node samplers.
+
+        Return
+        ------
+        self : Node2Vec
+        """
+        A = utils.to_adjacency_matrix(net)
+        self.sampler.sampling(A)
+        return self
+
+    def update_embedding(self, dim):
+        # Update the dimension and train the model
+        # Sample the sequence of nodes using a random walk
+        self.w2vparams["window"] = self.sampler.window_length
+
+        self.sentences = utils.walk2gensim_sentence(
+            self.sampler.walks, self.sampler.window_length
+        )
+
+        self.w2vparams["size"] = dim
+        self.model = gensim.models.Word2Vec(sentences=self.sentences, **self.w2vparams)
+
+        num_nodes = len(self.model.wv.index2word)
+        self.in_vec = np.zeros((num_nodes, dim))
+        self.out_vec = np.zeros((num_nodes, dim))
+        for i in range(num_nodes):
+            if "%d" % i not in self.model.wv:
+                continue
+            self.in_vec[i, :] = self.model.wv["%d" % i]
+            self.out_vec[i, :] = self.model.syn1neg[self.model.wv.vocab["%d" % i].index]
+
+
+class DeepWalk(Node2Vec):
+    def __init__(self, **params):
+        Node2Vec.__init__(self, **params)
+        self.w2vparams = {
+            "sg": 0,
+            "hs": 1,
+            "min_count": 0,
+            "workers": 4,
+        }
+
+class LaplacianEigenMap(NodeEmbeddings):
+    def __init__(self):
+        self.in_vec = None
+        self.L = None
+        self.deg = None
+
+    def fit(self, G):
+        A = utils.to_adjacency_matrix(G)
+
+        # Compute the (inverse) normalized laplacian matrix
+        deg = np.array(A.sum(axis=1)).reshape(-1)
+        Dsqrt = sparse.diags(1 / np.maximum(np.sqrt(deg), 1e-12), format="csr")
+        L = Dsqrt @ A @ Dsqrt
+
+        self.L = L
+        self.deg = deg
+        return self
+
+    def transform(self, dim, return_out_vector=False):
+        if self.in_vec is None:
+            self.update_embedding(dim)
+        elif self.in_vec.shape[1] != dim:
+            self.update_embedding(dim)
+        return self.in_vec
+
+    def update_embedding(self, dim):
+        u, s, _ = rsvds.rSVD(self.L, dim + 1)  # add one for the trivial solution
+        order = np.argsort(s)[::-1][1:]
+        u = u[:, order]
+
+        Dsqrt = sparse.diags(1 / np.maximum(np.sqrt(self.deg), 1e-12), format="csr")
+        self.in_vec = Dsqrt @ u
+        self.out_vec = u
---
+++ libs/embcom/embcom/samplers.py
@@ -0,0 +1,559 @@
+"""This module contains sampler class which generates a sequence of nodes from
+a network using a random walk.
+
+All samplers should be the subclass of NodeSampler class.
+"""
+import logging
+from abc import ABCMeta, abstractmethod
+from collections import Counter
+
+import faiss
+import numpy as np
+from embcom import utils
+from scipy import sparse
+from sklearn.cluster import KMeans
+
+import numba
+from numba import prange
+
+logger = logging.getLogger(__name__)
+
+class NodeSampler(metaclass=ABCMeta):
+    """Super class for node sampler class.
+
+    Implement
+        - get_trans_matrix
+        - sampling
+
+    Optional
+        - get_decomposed_trans_matrix
+    """
+
+    @abstractmethod
+    def get_trans_matrix(self, scale="normal"):
+        """Construct the transition matrix for the node sequence.
+
+        Return
+        ------
+        trans_prob : sparse.csr_matrix
+            Transition matrix. trans_prob[i,j] is the probability
+            that a random walker in node i moves to node j.
+        """
+
+    def get_trans_prob(self, src, trg):
+        """Construct the transition matrix for the node sequence.
+
+        Parameters
+        ----------
+        src : numpy.ndarray
+            IDs of source nodes
+        trg : numpy.ndarray
+            IDs of target nodes
+
+
+        Return
+        ------
+        trans_prob : numpy.ndarray
+            Transition probability
+        """
+        P = self.get_trans_matrix()
+        return np.array(P[(src, trg)]).reshape(-1)
+
+    @abstractmethod
+    def sampling(self):
+        """Generate a sequence of walks over the network.
+
+        Return
+        ------
+        walks : numpy.ndarray (number_of_walks, number_of_steps)
+            Each row indicates a trajectory of a random walker
+            walk[i,j] indicates the jth step for walker i.
+        """
+
+    @abstractmethod
+    def get_center_context_pairs(self, num_walks=5):
+        """get center and context pairs."""
+
+    @abstractmethod
+    def sample_context(self, pos_pairs, sz):
+        """sample context from center."""
+
+    @abstractmethod
+    def count_center_context_pairs(self):
+        """count the number of possible pairs."""
+
+    @abstractmethod
+    def get_batch_pairs(self, batch_size):
+        """get batch of center context pairs."""
+
+    def get_decomposed_trans_matrix(self, scale="normal"):
+        return [[self.get_trans_matrix(scale)]]
+
+
+#
+# SimpleWalk Sampler
+#
+class SimpleWalkSampler(NodeSampler):
+    def __init__(
+        self,
+        num_walks=10,
+        walk_length=80,
+        window_length=10,
+        restart_prob=0,
+        p=1.0,
+        q=1.0,
+        verbose=False,
+        sample_center_context_pairs=True,
+        random_teleport=False,
+        **params
+    ):
+        """Simple walk with restart.
+
+        Parameters
+        ----------
+        num_walks : int (Optional; Default 1)
+            Number of walkers to simulate for each randomized network.
+            A larger value removes the bias better but takes more time.
+        walk_length : int
+            Number of steps for a single walker
+        p : float
+            Parameter for the node2vec
+        q : float
+            Parameter for the node2vec
+        window_length : int
+            Size of the window
+        restart_prob : float
+            Probability of teleport back to the starting node
+        verbose : bool
+            Set true to display the progress (NOT IMPLEMENTED)
+        """
+
+        self.restart_prob = restart_prob
+        self.trans_prob = None
+        self.verbose = verbose
+        self.num_nodes = -1
+
+        # parameters for random walker
+        self.num_walks = int(num_walks)
+        self.walk_length = walk_length
+        self.sample_center_context_pairs = sample_center_context_pairs
+        self.p = p
+        self.q = q
+        self.walks = None
+        self.W = None
+        self.cum_trans_prob = None
+
+        # parameters for learning embeddings
+        self.window_length = window_length
+        self.random_teleport = random_teleport
+
+    def _init_center_context_pairs(self, center, context, freq):
+        self.W = sparse.csr_matrix(
+            (freq, (center, context)), shape=(self.num_nodes, self.num_nodes),
+        )
+        self.cum_trans_prob = calc_cum_trans_prob(self.W.copy())
+        self.center_prob = np.array(self.W.sum(axis=1)).reshape(-1)
+        if ~np.isclose(np.sum(self.center_prob), 0):
+            self.center_prob /= np.sum(self.center_prob)
+
+    def sampling(self, net):
+        self.num_nodes = net.shape[0]
+        self.A = net
+
+        if np.isclose(self.p, 1) and np.isclose(self.q, 1):
+            if self.sample_center_context_pairs:
+                center, context, freq = sample_center_context_pair(
+                    self.A,
+                    self.num_walks,
+                    self.walk_length,
+                    self.restart_prob,
+                    self.window_length,
+                    random_teleport=self.random_teleport,
+                )
+                self._init_center_context_pairs(center, context, freq)
+            else:
+                self.walks = simulate_simple_walk(
+                    self.A,
+                    self.num_walks,
+                    self.walk_length,
+                    self.restart_prob,
+                    random_teleport=self.random_teleport,
+                )
+                self.walks = self.walks.astype(int)
+        else:  # biased walk
+            # calc supra-adjacency matrix
+            Asupra, node_pairs = utils.construct_line_net_adj(net, p=self.p, q=self.q)
+            self.num_nodes_supra = Asupra.shape[0]
+
+            # Find the starting node ids
+            start_node_ids = np.where(node_pairs[:, 0] == node_pairs[:, 1])[0]
+            self.walks = simulate_simple_walk(
+                Asupra,
+                self.num_walks,
+                self.walk_length,
+                self.restart_prob,
+                start_node_ids=start_node_ids,
+            )
+            self.walks = self.walks.astype(int)
+
+            # Convert supra-node id to the id of the nodes in the original net
+            self.walks = node_pairs[self.walks.reshape(-1), 1].reshape(self.walks.shape)
+
+    def get_trans_matrix(self, scale="normal"):
+        """Compute the transition probability from one node to another.
+
+        Return
+        ------
+        trans_prob : numpy.ndarray
+            Transition probability matrix of size.
+            (number of nodes, number of nodes).
+            trans_prob[i,j] indicates the transition probability
+            from node i to node j.
+        """
+
+        # Generate a sequence of random walks
+        if self.W is None:
+            self.get_center_context_pairs()
+        if self.joint_prob is False:
+            trans_prob = utils.to_trans_mat(self.W)
+        if scale == "log":
+            trans_prob.data = utils.safe_log(trans_prob.data)
+        return trans_prob
+
+    def get_center_context_pairs(self):
+        if self.W is None:
+            (center, context, freq,) = generate_center_context_pair(
+                self.walks, self.window_length
+            )
+            self._init_center_context_pairs(center, context, freq)
+            return center, context, freq
+        else:
+            center, context, freq = sparse.find(self.W)
+            return center, context, freq
+
+    def sample_context(self, center, sz):
+        context = sample_columns_from_cum_prob(
+            np.repeat(center, sz),
+            self.cum_trans_prob.indptr,
+            self.cum_trans_prob.indices,
+            self.cum_trans_prob.data,
+        )
+        context = context.reshape((len(center), sz))
+        return context
+
+
+#
+# Helper function 
+#
+def calc_cum_trans_prob(A):
+    P = A.copy()
+    a = _calc_cum_trans_prob(P.indptr, P.indices, P.data.astype(float), P.shape[0])
+    P.data = a
+    return P
+
+# @numba.jit(nopython=True, parallel=True)
+def _calc_cum_trans_prob(
+    A_indptr, A_indices, A_data_, num_nodes,  # should be cumulative
+):
+    A_data = A_data_.copy()
+    for i in range(num_nodes):
+        # Compute the out-deg
+        outdeg = np.sum(A_data[A_indptr[i] : A_indptr[i + 1]])
+        A_data[A_indptr[i] : A_indptr[i + 1]] = np.cumsum(
+            A_data[A_indptr[i] : A_indptr[i + 1]]
+        ) / np.maximum(outdeg, 1)
+    return A_data
+
+
+@numba.jit(nopython=True, parallel=True)
+def sample_columns_from_cum_prob(rows, A_indptr, A_indices, A_data):
+    retvals = -np.ones(len(rows))
+    for i in range(len(rows)):
+        r = rows[i]
+        nnz_row = A_indptr[r + 1] - A_indptr[r]
+        if nnz_row == 0:
+            continue
+
+        # find a neighbor by a roulette selection
+        _ind = np.searchsorted(
+            A_data[A_indptr[r] : A_indptr[r + 1]], np.random.rand(), side="right",
+        )
+        retvals[i] = A_indices[A_indptr[r] + _ind]
+    return retvals
+
+
+def simulate_simple_walk(
+    A,
+    num_walk,
+    walk_length,
+    restart_prob,
+    start_node_ids=None,
+    restart_at_dangling=False,
+    is_cum_trans_prob_mat=False,
+    random_teleport=False,
+):
+    """Wrapper for."""
+
+    if A.getformat() != "csr":
+        raise TypeError("A should be in the scipy.sparse.csc_matrix")
+
+    if start_node_ids is None:
+        start_node_ids = np.arange(A.shape[0])
+
+    # Extract the information on the csr matrix
+    logger.debug(
+        "simulate random walks: network of {} nodes. {} walkers per node. {} start nodes".format(
+            A.shape[0], num_walk, len(start_node_ids)
+        )
+    )
+    if is_cum_trans_prob_mat is False:
+        logger.debug("Calculating the transition probability")
+        P = calc_cum_trans_prob(A)
+    else:
+        P = A
+
+    logger.debug("Start simulation")
+    return _simulate_simple_walk(
+        P.indptr,
+        P.indices,
+        P.data.astype(float),
+        P.shape[0],
+        float(restart_prob),
+        np.repeat(start_node_ids, num_walk),
+        walk_length,
+        restart_at_dangling,
+        random_teleport,
+    )
+
+
+# @numba.jit(nopython=True, parallel=True)
+@numba.jit(nopython=True, cache=True, parallel=True)
+def _simulate_simple_walk(
+    A_indptr,
+    A_indices,
+    A_data,  # should be cumulative
+    num_nodes,
+    restart_prob,
+    start_node_ids,
+    walk_length,
+    restart_at_dangling,
+    random_teleport,
+):
+    """Sampler based on a simple walk. A random walker chooses a neighbor with
+    proportional to edge weights. For a fast simulation of random walks, we
+    exploit scipy.sparse.csr_matrix data structure. In scipy.sparse.csr_matrix
+    A, there are 3 attributes:
+
+    - A.indices : column ID
+    - A.data : element value
+    - A.indptr : the first index of A.indices for the ith row
+    The neighbors of node i are given by A.indices[A.indptr[i]:A.indptr[i+1]], and
+    the edge weights are given by A.data[A.indptr[i]:A.indptr[i+1]].
+    Parameters
+    ----------
+    A: scipy.sparse.csr_matrix
+        Adjacency matrix of a network
+    walk_length: int
+        Length of a single walk
+    window_length: int
+        Length of a window rolling over a generated sentence
+    restart_prob: float (Optional; Default 0)
+        Restart probability
+    return_sentence bool (Optional; Default True)
+        Return sentence in form of numpy.ndarray. Set false if using residual2vec
+            Returns
+    -------
+    sampler: func
+        A function that takes start node ids (numpy.ndarray) as input.
+        If return_sentence is True, sampler returns sentences in form of numpy.ndarray
+        where sentence[n,t] indicates the tth word in the nth sentence.
+        If return_sentence is False, sampler returns center-context node pairs and its frequency:
+        - center: numpy.ndarray
+            Center words
+        - context: numpy.ndarray
+            Context words
+        - freq: numpy.ndarray
+            Frequency
+    """
+    # Alocate a memory for recording a walk
+    walks = -np.ones((len(start_node_ids), walk_length), dtype=np.int32)
+    for sample_id in prange(len(start_node_ids)):
+        start_node = start_node_ids[sample_id]
+        # Record the starting node
+        visit = start_node
+        walks[sample_id, 0] = visit
+        for t in range(1, walk_length):
+            # Compute the number of neighbors
+            outdeg = A_indptr[visit + 1] - A_indptr[visit]
+            # If reaches to an absorbing state, finish the walk
+            # the random walker is teleported back to the starting node
+            # or Random walk with restart
+            if outdeg == 0:
+                if restart_at_dangling:
+                    if random_teleport:
+                        next_node = np.random.randint(0, num_nodes)
+                    else:
+                        next_node = start_node
+                else:
+                    if t == 1:  # when starting from sink
+                        pass
+                        walks[sample_id, t] = visit
+                    break
+            elif np.random.rand() <= restart_prob:
+                if random_teleport:
+                    next_node = np.random.randint(0, num_nodes)
+                else:
+                    next_node = start_node
+            else:
+                # find a neighbor by a roulette selection
+                _next_node = np.searchsorted(
+                    A_data[A_indptr[visit] : A_indptr[visit + 1]],
+                    np.random.rand(),
+                    side="right",
+                )
+                next_node = A_indices[A_indptr[visit] + _next_node]
+            # Record the transition
+            walks[sample_id, t] = next_node
+            # Move
+            visit = next_node
+    return walks
+
+
+def sample_center_context_pair(
+    A,
+    num_walks,
+    walk_length,
+    restart_prob,
+    window_length,
+    random_teleport=False,
+    batch_size=100000,
+):
+
+    num_nodes = A.shape[0]
+    num_chunk = np.ceil(num_walks * num_nodes / batch_size).astype(int)
+    pair_cnt = Counter()
+
+    logger.debug("Calculating the transition probability")
+    P = calc_cum_trans_prob(A)
+    for ch_id, start_node_ids in enumerate(
+        np.array_split(np.arange(num_nodes), num_chunk)
+    ):
+        logger.debug("chunk {} / {}: simulate random walk".format(ch_id, num_chunk))
+        walks = simulate_simple_walk(
+            P,
+            num_walks,
+            walk_length,
+            restart_prob,
+            start_node_ids=start_node_ids,
+            is_cum_trans_prob_mat=True,
+            random_teleport=random_teleport,
+        )
+
+        walk_length = walks.shape[1]
+        walk_num = walks.shape[0]
+        L_single = window_length * (walk_length - window_length)
+        L_all = walk_num * L_single
+
+        logger.debug(
+            "chunk {} / {}: generating {} center context pairs".format(
+                ch_id, num_chunk, L_all
+            )
+        )
+        walks = walks.astype(int)
+        pairs = _generate_center_context_pair_ids(walks, window_length)
+        pair_cnt += Counter(pairs)
+        logger.debug(
+            "chunk {} / {}: {} pairs sampled in total".format(
+                ch_id, num_chunk, len(pair_cnt)
+            )
+        )
+
+    ids = np.fromiter(pair_cnt.keys(), dtype=int)
+    freq = np.fromiter(pair_cnt.values(), dtype=float)
+    w = np.floor((np.sqrt(8 * ids + 1) - 1) * 0.5)
+    t = (w ** 2 + w) * 0.5
+    context = ids - t
+    center = w - context
+    return center.astype(int), context.astype(int), freq
+
+
+@numba.jit(nopython=True, cache=True, parallel=True)
+def _generate_center_context_pair_ids(walks, window_length):
+
+    """Generate center context node pairs from walks."""
+    #
+    # Allocate a memory for center and context node
+    #
+    walk_length = walks.shape[1]
+    walk_num = walks.shape[0]
+
+    L_single = window_length * (walk_length - window_length)
+    L_all = walk_num * L_single
+    pairs = -np.ones(L_all)
+
+    #
+    # Simulate the random walk
+    #
+    for sample_id in prange(walk_num):
+        # Tie center and context nodes
+        # Two node ids are converted to a single int id using the Canter pairng
+        for t in range(L_single):
+            t0, t1 = divmod(t, window_length)
+            pid = sample_id * L_single + t
+            t1 = t0 + t1 + 1
+            if (walks[sample_id, t0] < 0) or (walks[sample_id, t1] < 0):
+                continue
+            # Cantor pairing function
+            pairs[pid] = int(walks[sample_id, t0]) + int(walks[sample_id, t1])  #
+            pairs[pid] = pairs[pid] * (pairs[pid] + 1) / 2 + int(walks[sample_id, t1])
+    pairs = pairs[pairs >= 0]
+    return pairs
+
+
+def generate_center_context_pair(walks, window_length):
+    return _generate_center_context_pair(walks.astype(np.int64), int(window_length))
+
+
+@numba.jit(nopython=True, cache=True, parallel=True)
+def _generate_center_context_pair(walks, window_length):
+    """Generate center context node pairs from walks."""
+
+    #
+    # Allocate a memory for center and context node
+    #
+    walk_length = walks.shape[1]
+    walk_num = walks.shape[0]
+
+    L_single = window_length * (walk_length - window_length)
+    L_all = walk_num * L_single
+    pairs = -np.ones(L_all)
+
+    #
+    # Simulate the random walk
+    #
+    for sample_id in prange(walk_num):
+
+        # Tie center and context nodes
+        # Two node ids are converted to a single int id using the Canter pairng
+        for t in range(L_single):
+            t0, t1 = divmod(t, window_length)
+            pid = sample_id * L_single + t
+            t1 = t0 + t1 + 1
+            if (walks[sample_id, t0] < 0) or (walks[sample_id, t1] < 0):
+                continue
+            # Cantor pairing function
+            pairs[pid] = int(walks[sample_id, t0]) + int(walks[sample_id, t1])  #
+            pairs[pid] = pairs[pid] * (pairs[pid] + 1) / 2 + int(walks[sample_id, t1])
+    pairs = pairs[pairs >= 0]
+
+    # Count center-context pairs
+    freq = np.bincount(pairs.astype(np.int64))
+    ids = np.nonzero(freq)[0]
+    freq = freq[ids]
+
+    # Deparing
+    w = np.floor((np.sqrt(8 * ids + 1) - 1) * 0.5)
+    t = (w ** 2 + w) * 0.5
+    context = ids - t
+    center = w - context
+    return center, context, freq
