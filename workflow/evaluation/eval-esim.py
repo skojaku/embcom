326bb03c08844e923bfa4532c61683675234a258
Sadamori Kojaku <skojaku@iu.edu>
2022-04-07 13:08:52 -0400

    corrected elementcentric similarity

--- workflow/evaluation/eval-esim.py
+++ workflow/evaluation/eval-esim.py
@@ -12,8 +12,8 @@ if "snakemake" in sys.modules:
     com_file = snakemake.input["com_file"]
     output_file = snakemake.output["output_file"]
 else:
-    com_file = "../../data/multi_partition_model/networks/node_n~2500_K~2_cave~20_mu~0.5_sample~0.npz"
-    detected_group_file = "../../data/multi_partition_model/communities/clus_n~2500_K~2_cave~20_mu~0.5_sample~0_model_name~node2vec-matrixfact-limit_window_length~10_dim~0_metric~cosine_clustering~kmeans.npz"
+    com_file = "../../data/multi_partition_model/networks/node_n~2500_K~16_cave~50_mu~0.5_sample~0.npz"
+    detected_group_file = "../../data/multi_partition_model/communities/clus_n~2500_K~16_cave~50_mu~0.1_sample~0_model_name~node2vec-matrixfact-limit_window_length~10_dim~0_metric~cosine_clustering~kmeans.npz"
     output_sim_file = "unko"
 
 
@@ -33,27 +33,28 @@ def calc_esim(y, ypred):
 
     Ka, Kb = len(ylab), len(ypredlab)
 
-    K = int(np.maximum(np.max(y), np.max(ypred))) + 1
-    M = len(y)
-    UA = sparse.csr_matrix((np.ones_like(y), (np.arange(y.size), y)), shape=(M, K))
+    K = np.maximum(Ka, Kb)
+    N = len(y)
+    UA = sparse.csr_matrix((np.ones_like(y), (np.arange(y.size), y)), shape=(N, K))
     UB = sparse.csr_matrix(
-        (np.ones_like(ypred), (np.arange(ypred.size), ypred)), shape=(M, K)
+        (np.ones_like(ypred), (np.arange(ypred.size), ypred)), shape=(N, K)
     )
 
-    fA = np.array(UA.sum(axis=0)).reshape(-1)
-    fB = np.array(UB.sum(axis=0)).reshape(-1)
+    nA = np.array(UA.sum(axis=0)).reshape(-1)
+    nB = np.array(UB.sum(axis=0)).reshape(-1)
 
-    fAB = UA.T @ UB
+    nAB = (UA.T @ UB).toarray()
+    nAB_rand = np.outer(nA, nB) / N
 
-    S = 0
-    for i in range(len(y)):
-        S += (fAB[y[i], ypred[i]] ** 2) * np.minimum(
-            1 / np.maximum(1, fA[y[i]]), 1 / np.maximum(1, fB[ypred[i]])
-        )
-    S /= M
+    # Calc element-centric similarity
+    Q = np.maximum(nA[:, None] @ np.ones((1, K)), np.ones((K, 1)) @ nB[None, :])
+    Q = 1 / np.maximum(Q, 1)
+    S = np.sum(np.multiply(Q, (nAB ** 2))) / N
 
-    Srand = np.minimum(1 / Ka, 1 / Kb)
-
+    # Calc the expected element-centric similarity for random partitions
+    Q = np.maximum(nA[:, None] @ np.ones((1, K)), np.ones((K, 1)) @ nB[None, :])
+    Q = 1 / np.maximum(Q, 1)
+    Srand = np.sum(np.multiply(Q, (nAB_rand ** 2))) / N
     Scorrected = (S - Srand) / (1 - Srand)
     return Scorrected
 
@@ -66,5 +67,3 @@ score_esim = calc_esim(memberships, group_ids)
 res_table = pd.DataFrame([{"score": score_esim, "score_type": "esim"}]).to_csv(
     output_file, index=False
 )
-
-# %%
