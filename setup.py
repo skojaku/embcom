c052205a690f8d2452fe0047db43a45f33f05f37
SADAMORI KOJAKU <skojaku@iu.edu>
2024-11-14 05:04:03 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name="embcom",
+    version="0.1",
+    description="embcom",
+    author="...",
+    packages=["embcom"],
+)
